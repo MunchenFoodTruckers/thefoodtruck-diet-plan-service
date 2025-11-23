30b41022e0ce0ea266f058b05d6d5bccec5db71c
San <sandeeppaul11111@gmail.com>
2025-11-23 11:58:13 +0100

    fix: correct module import names

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,12 +1,12 @@
-import { Module} from "@nestjs/common";
+import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
-import { DietPlanModule } from "./diet-plan/diet-plan.module";
+import { UdietUplanModule } from "./diet-plan/diet-plan.module";
 import { PrismaService } from "./prisma.service";
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), DietPlanModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), UdietUplanModule],
   controllers: [],
   providers: [PrismaService],
   exports: [PrismaService]
 })
-export class AppModule {}
+export class AppModule { }
